85d0033a1bfc5bc963687786063ff8f17749158c
Zach McArtor <zmcartor@gmail.com>
2017-02-25 22:48:11 -0700

    objects-at function

--- wizard-game.lisp
+++ wizard-game.lisp
@@ -1,5 +1,5 @@
 (defvar *edges* '( ( living-room (garden west door) (attic upstairs ladder))
-(garden (living room east door)) (attic  (living-room downstairs ladder))))
+(garden (living-room east door)) (attic  (living-room downstairs ladder))))
 
 ;; notice the back-tic to note this is string interpolation
 ;; backticks and commas flip-flop from data/code mode
@@ -10,3 +10,20 @@
 ;; and append all description into one large list
 (defun describe-paths (location edges)
    (apply #'append (mapcar #'describe-path (cdr assoc location edges))))
+
+;; list of all objects
+(defparameter *objects* '(whiskey bucket frog chain))
+
+;; alist of various objects and their locations
+;; alist - a list of pairs ( (pairs) (pairs .. )
+(defparameter *object-locations* '( (whiskey living-room) (bucket living-room)
+                                    (chain garden) (frog garden)))
+;; list objects from a given location
+;; pass in list of all objects, list of all object locations
+;; return list of all objects at a specified location.
+;; could likely generate list of objects from objs-locs
+
+(defun objects-at (loc objs obj-locs)
+   (labels ((at-loc-p(obj)
+         (eq (cadr  (assoc obj obj-locs)) loc)))
+   (remove-if-not #'at-loc-p objs)))
